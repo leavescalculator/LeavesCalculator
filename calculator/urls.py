53d2d158f853984e2cb2c4175f291948808e50b8
stevenborrego <steven@Stevens-MacBook-Pro.local>
2019-10-10 19:04:17 -0700

    delete tutorial files and update directory names

--- calculator/urls.py
+++ calculator/urls.py
@@ -18,7 +18,6 @@ from django.urls import include, path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
