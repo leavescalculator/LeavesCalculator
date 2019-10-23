0235e12273a5957c0c6cdaa900cd311575357970
stevenborrego <steven@Stevens-MacBook-Pro.local>
2019-10-22 19:26:08 -0700

    add base vue components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,43 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//import the vue instance
 import Vue from 'vue'
-import App from './App.vue'
+//import the App component
+import App from './App'
+//import the vue router
+import VueRouter from 'vue-router'
+//tell vue to use the router
+Vue.use(VueRouter)
+//define your routes
+//import the login component
+import Login from './components/Login'
+//import the login component
+import Questions from './components/Questions'
+//import the report component
+import Report from './components/Report'
+const routes = [
+//route for the home route of the web page
+{ path: '/', component: Login },
+{ path: '/questions', component: Questions },
+//route for the about route of the web page
+{ path: '/report', component: Report }
+]
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+// Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes, // short for routes: routes
+  mode: 'history'
 })
+//instatinat the vue instance
+new Vue({
+//define the selector for the root component
+  el: '#app',
+  //pass the template to the root component
+  template: '<App/>',
+  //declare components that the root component can access
+  components: { App },
+  //pass in the router to the Vue instance
+  router
+}).$mount('#app')//mount the router on the app
