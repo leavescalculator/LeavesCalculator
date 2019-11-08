f410c1567c3309744e5577f04976ab19e0478186
Ian <imcbride@pdx.edu>
2019-11-07 20:17:19 -0800

    Add parsing for dbnode option titles

--- src/components/DatabaseQuestion.vue
+++ src/components/DatabaseQuestion.vue
@@ -9,10 +9,52 @@
 <script>
 export default {
     data: () => ({
-
     }),
-    props: ['title', 'options'],
-    methods: {}
+    props: ['title', 'options', 'user'],
+    methods: {
+        //Assumes the option.title of any db node is structured in a specific way
+        //"OPP prop value" ie "== fmla_eligibility T"
+        parse(str) {
+            let args = str.split(' ');
+            switch (args[0]) {
+                case '==':
+                    if (this.user.hasOwnProperty(args[1]))
+                    {
+                        return this.user[args[1]] === args[2];
+                    }
+                    break;
+                case '<':
+                    if (this.user.hasOwnProperty(args[1]))
+                    {
+                        return this.user[args[1]] < args[2];
+                    }
+                    break;
+                case '>=':
+                    if (this.user.hasOwnProperty(args[1]))
+                    {
+                        return this.user[args[1]] >= args[2];
+                    }
+                    break;
+                case 'else':
+                    return true;
+                default:
+                    return false;
+
+            }
+            return false;
+        }
+    },
+    mounted() {
+      for (let i in options.length) {
+          if (this.options[i].hasOwnProperty('title')) {
+              if (parse(options[i].title)) {
+                  this.$emit('option-selected', i);
+                  return;
+              }
+          }
+
+      }
+    }
 
 
 }
