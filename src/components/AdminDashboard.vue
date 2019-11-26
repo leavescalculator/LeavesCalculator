0c510cd27881d2fa35b3c4ffed22ad96ac874bf7
angelicphan <phan4@pdx.edu>
2019-11-26 00:16:40 -0800

    remove styling

--- src/components/AdminDashboard.vue
+++ src/components/AdminDashboard.vue
@@ -565,11 +565,4 @@ export default {
 #legend {
   height: 140px;
 }
-
-#graphTitle {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
 </style>
