188ff64a477131fc2bca363eac34eb7d3a7a97e2
angelicphan <phan4@pdx.edu>
2019-11-25 23:54:32 -0800

    got rid of text box and jsonOrPositions stuff

--- src/components/AdminDashboard.vue
+++ src/components/AdminDashboard.vue
@@ -11,13 +11,7 @@
         ability to save and load graphs from the database.
       -->
       <div class="col-5">
-        <div id="jsonOrPositions" class="form-control" contenteditable="true">{{ jsonOrPositions }}</div>
-        <br />
         <div class="btn-group" role="group">
-          <button class="btn btn-info" @click="outputJson">Output JSON</button>
-          <button class="btn btn-info" @click="loadJson">Load JSON</button>
-          <button class="btn btn-info" @click="outputPositions">Output Positions</button>
-          <button class="btn btn-info" @click="loadPositions">Load Positions</button>
           <button class="btn btn-info" @click="saveAsNewGraph">Save As New Graph</button>
           <button class="btn btn-info" @click="saveGraph">Update Graph</button>
           <button class="btn btn-info" @click="activateGraph">Activate Graph</button>
@@ -201,16 +195,11 @@ import popper from "cytoscape-popper";
 cytoscape.use(popper);
 // The styling for the Cytoscape.js graph
 import graph_style from "../assets/graph-style.json";
-// The default graph representation
 
 export default {
   name: "admin-dashboard",
   props: ["isAdmin", "Nodes", "graph-status", "graph-id", "Cords"],
   data: () => ({
-    // The data stored in the large textbox within the header
-    // TODO: this field can be removed when we are saving and loading
-    // from the DB
-    jsonOrPositions: "",
     // Will become an object with setters and getters for fields of the selected element on selection
     selectedElement: null,
     // The available input fields for a node
@@ -331,9 +320,8 @@ export default {
         }
       }
     },
-    // Creates the JSON structure to represent the graph based on the
-    // current state of the Cytoscape.js graph
-    outputJson() {
+    getGraphJson() {
+      //This function will get the nodes of the graph and its information in JSON
       let output = {};
       let nodes = this.cy.$(".graph-node");
       for (let node = 0; node < nodes.length; node++) {
@@ -362,23 +350,7 @@ export default {
           }
         }
       }
-      this.jsonOrPositions = JSON.stringify(output);
-    },
-    // Gets the positions of every graph node
-    outputPositions() {
-      let output = [];
-      let nodes = this.cy.$(".graph-node");
-      for (let node = 0; node < nodes.length; node++) {
-        output.push(nodes[node].relativePosition());
-      }
-      this.jsonOrPositions = JSON.stringify(output);
-    },
-    // Sets the positions for every graph node
-    loadPositions() {
-      let positions = this.cords;
-      for (let node = 0; node < positions.length; node++) {
-        this.cy.$(".graph-node")[node].relativePosition(positions[node]);
-      }
+      return output;
     },
     getPositions() {
       //This function will get the positions of the nodes of the graph in JSON
@@ -411,18 +383,6 @@ export default {
         this.ur.do("remove", this.cy.$(":selected")[0]);
         document.getElementById("elementInfo").style.visibility = "hidden";
       }
-    },
-    // Parses the JSON in the header's textbox and updates the graph's state
-    loadJson() {
-      this.cy.$(".graph-node").remove();
-      let nodes = JSON.parse(this.jsonOrPositions);
-      this.parseJson(nodes);
-      this.cy
-        .$(".graph-node, .graph-edge")
-        .layout({
-          name: "breadthfirst"
-        })
-        .run();
     },
     // Hides the popper element
     hideInfo() {
@@ -487,38 +447,6 @@ export default {
           Authorization: this.auth
         }
       });
-    },
-    getGraphJson() {
-      //This function will get the nodes of the graph and its information in JSON
-      let output = {};
-      let nodes = this.cy.$(".graph-node");
-      for (let node = 0; node < nodes.length; node++) {
-        let element = nodes[node];
-        let nodeId = element.data("id");
-        output[nodeId] = {
-          title: element.data("title"),
-          input: element.data("input"),
-          options: []
-        };
-        if (output[nodeId].input === "display") {
-          output[nodeId].description = element.data("description");
-        }
-        let edges = this.cy.edges('[source = "' + nodeId + '"]');
-        for (let edge = 0; edge < edges.length; edge++) {
-          element = edges[edge];
-          output[nodeId].options.push({
-            title: element.data("title"),
-            add_time: element.data("add_time"),
-            next_node: element.data("target")
-          });
-          if (output[nodeId].input === "button-descriptive") {
-            output[nodeId].options[edge].description = element.data(
-              "description"
-            );
-          }
-        }
-      }
-      return output;
     },
     // Shows the popper element, which displays and allows the changing
     // of specific graph elements.
@@ -634,15 +562,6 @@ export default {
   border: 2px solid #ccc;
   background: #fff;
   padding: 10px;
-}
-
-/* For the large textbox in the header
-   * TODO: Remove this when removing the textbox
-   */
-#jsonOrPositions {
-  height: 85px;
-  width: 490px;
-  overflow-y: auto;
 }
 
 #legend {
